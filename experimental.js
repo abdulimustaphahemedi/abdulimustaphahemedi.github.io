d1dfceb1e8e286764e28e110f5b766919a60cf33
abdulimustaphahemedi <107766528+abdulimustaphahemedi@users.noreply.github.com>
2023-03-17 07:01:47 +0300

    additional adjustment

---
+++ experimental.js
@@ -0,0 +1,10 @@
+const language = document.querySelector("#language")
+const languageItems = document.querySelector("#language-items")
+
+
+language.addEventListener("click", function(){
+    if(languageItems.style.display==="flex"){
+        languageItems.style.
+    }
+    languageItems.style.display = "flex"  
+});
