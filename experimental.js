2125316915e355041ec0f54b6f930ddd4702c306
abdulimustaphahemedi <107766528+abdulimustaphahemedi@users.noreply.github.com>
2023-03-17 18:15:34 +0300

    add another page

--- experimental.js
+++ experimental.js
@@ -1,10 +1,16 @@
 const language = document.querySelector("#language")
 const languageItems = document.querySelector("#language-items")
-
+const body = document.querySelector("main")
+const languageType = document.querySelector(".list")
 
 language.addEventListener("click", function(){
     if(languageItems.style.display==="flex"){
-        languageItems.style.
+        languageItems.style.display = "none"
+    }else{
+        languageItems.style.display = "flex"
     }
-    languageItems.style.display = "flex"  
+});
+
+body.addEventListener("mouseover", function(){
+    languageItems.style.display="none"
 });
