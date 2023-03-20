8c91b4ab41d12aca3c903b488407dde6f43b0c9b
abdulimustaphahemedi <107766528+abdulimustaphahemedi@users.noreply.github.com>
2023-03-20 16:10:06 +0300

    more at header

--- experimental.js
+++ experimental.js
@@ -1,7 +1,11 @@
 const language = document.querySelector("#language")
 const languageItems = document.querySelector("#language-items")
-const body = document.querySelector("main")
+const body = document.querySelector("body")
 const languageType = document.querySelector(".list")
+
+const search= document.querySelector("#search")
+const searchBar = document.querySelector("#searchText")
+const cancell = document.querySelector("#cancell")
 
 language.addEventListener("click", function(){
     if(languageItems.style.display==="flex"){
@@ -11,6 +15,22 @@ language.addEventListener("click", function(){
     }
 });
 
-body.addEventListener("mouseover", function(){
-    languageItems.style.display="none"
-});
+
+search.addEventListener("click", function(){
+
+        
+        if(searchBar.style.display === "none"){
+            searchBar.style.display = "flex";
+            search.style.display = "none";
+        }else{
+            searchBar.style.display = "none";
+        }
+
+
+   
+})
+
+cancell.addEventListener("click", function(){
+    searchBar.style.display = "none";
+    search.style.display = "flex";
+})
