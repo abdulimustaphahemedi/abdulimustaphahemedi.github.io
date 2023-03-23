1a8e03f8bc8a8c15bb113b31ae1d4730d6d00fb3
abdulimustaphahemedi <107766528+abdulimustaphahemedi@users.noreply.github.com>
2023-03-23 19:40:23 +0300

    more

--- car animation/game/car.js
+++ car animation/game/car.js
@@ -1,4 +1,22 @@
 const move = document.querySelector("#move");
+const body = document.querySelector("body");
+const car = document.querySelector(".car");
+const surface = document.querySelector(".surface");
+const Stop = document.querySelector("#stop");
+const Flashlight = document.querySelector("flashlight");
+const image = document.querySelector(".car-image");
+
+
 move.addEventListener("click", function(){
-    move.style.color= "red";
+    car.style.animation = "suspension 1s linear infinite";
+    surface.style.animation="moveright 6s linear infinite";
+});
+
+Stop.addEventListener("click", function(){
+    car.style.animation = "stop 1s linear infinite";
+    surface.style.animation="stop 6s linear infinite";
+})
+
+Flashlight.addEventListener("click", function(){
+    image.style.src ="../assets/Img_06.png";
 })
