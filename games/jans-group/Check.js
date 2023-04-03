5f1e231058ef29ac64ee16db633fdfc1eb7b4684
abdulimustaphahemedi <107766528+abdulimustaphahemedi@users.noreply.github.com>
2023-04-03 12:43:50 +0300

    udated

--- games/jans-group/Check.js
+++ games/jans-group/Check.js
@@ -1,6 +1,10 @@
 const  bt_truk = document.querySelector(".bt-truck");
+const  bt_truk2 = document.querySelector(".bt-truck2");
+
 const  check = document.querySelector(".ckeck-item");
 const  check_icon =  document.querySelector(".check-icon");
+const  check2 = document.querySelector(".ckeck-item2");
+const  check_icon2 =  document.querySelector(".check-icon2");
 
 
 
@@ -16,5 +20,19 @@ bt_truk.addEventListener("click", function(){
         check.setAttribute("data-visible", "visible")
         console.log("not visible")
         check_icon.setAttribute("aria-hidden", "false");
+    }
+})
+
+bt_truk2.addEventListener("click", function(){
+    if (check2.getAttribute("data-visible") === "visible")
+    {
+        check2.setAttribute("data-visible", "not-visible")
+        console.log("visible")
+        check_icon2.setAttribute("aria-hidden", "true");
+    }
+    else{
+        check2.setAttribute("data-visible", "visible")
+        console.log("not visible")
+        check_icon2.setAttribute("aria-hidden", "false");
     }
 })
